d7617642866fb822804ebedce819493aa513ad39
Igor Rodrigues <igor.ti.1983@gmail.com>
2020-01-25 17:37:18 -0300

    Refactoring the project

--- BRConselho.Avaliacao.Web.Api/Startup.cs
+++ BRConselho.Avaliacao.Web.Api/Startup.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(BRConselho.Avaliacao.Web.Api.Startup))]
+
 namespace BRConselho.Avaliacao.Web.Api
 {
     public partial class Startup
